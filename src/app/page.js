9eb6cea4ad98e1c3136a7340a94ac006a6edd83e
AndreyrM01 <rjostin954@gmail.com>
2023-11-04 12:47:04 -0600

    feat(add):"adding navigation to the page "

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import { Text } from '@chakra-ui/react'
+import Navigation from './componets/Navigation';
 
 export default function Home() {
   return( 
     <>
       <Text opacity="50%">HOla mundo</Text>
+      <Navigation />
     </>
   );
 }

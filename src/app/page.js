b07a913064136d24a4a8bd1cbe653993414bc35a
Vale <valeriams13@gmail.com>
2023-11-13 16:52:26 -0600

    Images

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,8 @@
-import { Text } from '@chakra-ui/react'
 import Navigation from './componets/Navigation';
 
 export default function Home() {
   return( 
     <>
-      <Text opacity="50%">HOla mundo</Text>
       <Navigation />
     </>
   );
